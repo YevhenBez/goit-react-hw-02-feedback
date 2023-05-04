a65f8cd785a3311469af531834fa16fe4f0404e1
YevhenBez <byxgalter@bigmir.net>
2023-05-04 23:15:28 +0300

    Импортнул библиотеку prop-types   и прописал при помощи неё проверку значений. Задание 1 Модуль 2 REACT
    Импортнул библиотеку prop-types   и прописал при помощи неё проверку значений. Задание 1 Модуль 2 REACT

--- src/components/FeedbackOptions.js
+++ src/components/FeedbackOptions.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import css from 'feedbackOptions.module.css';
+import PropTypes from 'prop-types';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
     options.map(btn => <button className={css.buttonOptions} key={btn} type="button" onClick={onLeaveFeedback}>{btn}</button>)
 );
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired
+}
 
 export default FeedbackOptions;
