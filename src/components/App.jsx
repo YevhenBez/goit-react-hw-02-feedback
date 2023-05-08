bf9aa7b041cfe2a72171ea9f63306c795b53da1f
YevhenBez <byxgalter@bigmir.net>
2023-05-08 19:57:12 +0300

    Переделал код согласно ТЗ, обернув в    <section > </section>     компонент   <Statistics>   и компонент    <FeedbackOptions>    Задание 1 Модуля 2   REACT   Полностью готово
    Переделал код согласно ТЗ, обернув в    <section > </section>     компонент   <Statistics>   и компонент    <FeedbackOptions>     Для этого в файле    Section.js     Реакт-Компонент    переделал на простую функцию, которая возвращает заголовок и  {children}    А функцию в файле   App.jsx   переделал на Реакт-компонент. Перекинул туда все импорты и код с файла   Section.js     И, как резюме, экспортнул данный Реакт-Компонент    Арр    А в файле   index.js   переделал импорт под   Реакт-Компонент    Арр  Задание 1 Модуля 2   REACT   Полностью готово

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,77 @@
+import React from 'react';
 import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Notification from './Notification';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      {/* React homework template */}
-      <Section title="Please leave feedback"
+class App extends React.Component {
+  static defaultProps = {
+        buttons: ['good', 'neutral', 'bad' ],
+    };
+
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    }
+
+    handleLeaveFeedback = (event) => {
+        const name = event.currentTarget.textContent;
         
-      />
-    </div>
-  );
+        this.setState(prevState => ({
+            [name]: prevState[name] + 1,
+        }));
+        
+        
+    };
+
+    countTotalFeedback = () => {
+        console.log(Object.values(this.state));
+
+        return Object.values(this.state).reduce((acc, option) => acc + option, 0);
+    }
+
+    countPositiveFeedbackPercentage = () => {
+        const { good } = this.state;
+        console.log(this.countTotalFeedback());
+        return Math.round((good / this.countTotalFeedback()) * 100) || 0;
+        
+    };
+    
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        {/* React homework template */}
+        <div>
+          <Section title="Please leave feedback">
+            <FeedbackOptions options={this.props.buttons} onLeaveFeedback={this.handleLeaveFeedback} />
+          </Section>
+          <Section title="Statistics">
+            {this.countTotalFeedback() ? (
+                <Statistics
+                        good={this.state.good}
+                        neutral={this.state.neutral}
+                        bad={this.state.bad}
+                        total={this.countTotalFeedback()}
+                        positivePercentage={this.countPositiveFeedbackPercentage()}
+                />
+              ) : (
+                <Notification message="There is no feedback" />
+              )}
+          </Section>
+        </div>
+      </div>
+    )
+  }
 };
+
+export default App;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,70 +1,10 @@
-import React from 'react';
-import FeedbackOptions from './FeedbackOptions';
-import Statistics from './Statistics';
-import Notification from './Notification';
-
-
-
-class Section extends React.Component {
-    static defaultProps = {
-        buttons: ['good', 'neutral', 'bad' ],
-    };
-
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    }
-
-    
-
-    
-    handleLeaveFeedback = (event) => {
-        const name = event.currentTarget.textContent;
-        
-        this.setState(prevState => ({
-            [name]: prevState[name] + 1,
-        }));
-        
-        
-    };
-
-    countTotalFeedback = () => {
-        console.log(Object.values(this.state));
-
-        return Object.values(this.state).reduce((acc, option) => acc + option, 0);
-    }
-
-    countPositiveFeedbackPercentage = () => {
-        const { good } = this.state;
-        console.log(this.countTotalFeedback());
-        return Math.round((good / this.countTotalFeedback()) * 100) || 0;
-        
-    };
-    
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <FeedbackOptions options={this.props.buttons} onLeaveFeedback={this.handleLeaveFeedback} />
-                <h2>Statistics</h2>
-                    <div>
-                        {this.countTotalFeedback() ? (
-                                <Statistics
-                                        good={this.state.good}
-                                        neutral={this.state.neutral}
-                                        bad={this.state.bad}
-                                        total={this.countTotalFeedback()}
-                                        positivePercentage={this.countPositiveFeedbackPercentage()}
-                                />
-                        ) : (
-                            <Notification message="There is no feedback" />
-                        )}
-                    </div>
-            </div>
-        )
-    }
-
+function Section({ title, children }) {
+    return (
+        <section >
+            <h2>{title}</h2>
+            {children}
+        </section>
+    );
 }
 
 export default Section;
