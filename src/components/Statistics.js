1c2df2769e0f14377d8cea2d86763489a9f88d21
YevhenBez <byxgalter@bigmir.net>
2023-05-10 12:49:38 +0300

    Отформатировал документ при помощи Притиер. Задание 1 Модуль 2 REACT
    Отформатировал документ при помощи Притиер. Задание 1 Модуль 2 REACT

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -3,21 +3,23 @@ import css from 'statistics.module.css';
 import PropTypes from 'prop-types';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
-    <div>
-        <span className={css.statistics}>Good:{good}</span>
-        <span className={css.statistics}>Neutral:{neutral}</span>
-        <span className={css.statistics}>Bad:{bad}</span>
-        <span className={css.statistics}>Total:{total}</span>
-        <span className={css.statistics}>Positive feedback:{positivePercentage}%</span>
-    </div>
+  <div>
+    <span className={css.statistics}>Good:{good}</span>
+    <span className={css.statistics}>Neutral:{neutral}</span>
+    <span className={css.statistics}>Bad:{bad}</span>
+    <span className={css.statistics}>Total:{total}</span>
+    <span className={css.statistics}>
+      Positive feedback:{positivePercentage}%
+    </span>
+  </div>
 );
 
 Statistics.propTypes = {
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-    total: PropTypes.number,
-    positivePercentage: PropTypes.number
-}
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
+};
 
 export default Statistics;
