b15d340ff2b015c7146d96c2c5752192b72fe376
YevhenBez <byxgalter@bigmir.net>
2023-05-10 12:43:22 +0300

    Отформатировал документ при помощи Притиер. Задание 1 Модуль 2 REACT
    Отформатировал документ при помощи Притиер. Задание 1 Модуль 2 REACT

--- src/components/FeedbackOptions.js
+++ src/components/FeedbackOptions.js
@@ -2,13 +2,21 @@ import React from 'react';
 import css from 'feedbackOptions.module.css';
 import PropTypes from 'prop-types';
 
-const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-    options.map(btn => <button className={css.buttonOptions} key={btn} type="button" onClick={onLeaveFeedback}>{btn}</button>)
-);
+const FeedbackOptions = ({ options, onLeaveFeedback }) =>
+  options.map(btn => (
+    <button
+      className={css.buttonOptions}
+      key={btn}
+      type="button"
+      onClick={onLeaveFeedback}
+    >
+      {btn}
+    </button>
+  ));
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired
-}
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
 
 export default FeedbackOptions;
