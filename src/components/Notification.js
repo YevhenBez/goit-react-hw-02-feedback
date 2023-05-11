10e4647f536e7a2aa0faade1098b2cf0235aa5ce
YevhenBez <byxgalter@bigmir.net>
2023-05-11 11:31:23 +0300

    Поставил всем ПропТайпам обязательность. Корректировка Задания 1 Модуля 2   REACT
    Поставил всем ПропТайпам обязательность при помощи     .isRequired     Корректировка Задания 1 Модуля 2   REACT

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Notification = ({ message }) => <h3>{message}</h3>;
 
 Notification.propTypes = {
-  message: PropTypes.string,
+  message: PropTypes.string.isRequired,
 };
 
 export default Notification;
